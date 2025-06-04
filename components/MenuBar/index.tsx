2cea6b15dbfd1c0985c0282cdeffff45f2885fbf
jmsuarez <jmsuarez23@yahoo.com>
2025-06-04 20:15:51 +0800

    Remove usage of Logo in MenuBar

--- components/MenuBar/index.tsx
+++ components/MenuBar/index.tsx
@@ -2,7 +2,6 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import styles from './MenuBar.module.css';
-import Logo from ''
 
 const MenuBar = () => {
   const [isOpen, setIsOpen] = useState(false);
